fd46fbe12924b7f5cce51b95d9e3282374809b1d
Warrick Corfe-Tan <warrickct@gmail.com>
2021-06-25 11:43:28 +1000

    Confirm button on left side of confirm modal button group

--- ts/components/session/SessionConfirm.tsx
+++ ts/components/session/SessionConfirm.tsx
@@ -102,6 +102,7 @@ const SessionConfirmInner = (props: SessionConfirmDialogProps) => {
       </div>
 
       <div className="session-modal__button-group">
+        <SessionButton text={okText} buttonColor={okTheme} onClick={onClickOkHandler} />
         {!hideCancel && (
           <SessionButton
             text={cancelText}
@@ -109,7 +110,6 @@ const SessionConfirmInner = (props: SessionConfirmDialogProps) => {
             onClick={onClickCancelHandler}
           />
         )}
-        <SessionButton text={okText} buttonColor={okTheme} onClick={onClickOkHandler} />
       </div>
     </SessionWrapperModal>
   );
